9cdfff7ebd776391cc2ad20adc913dcaa2bd8763
davidvatine <support@davidvatine.co.il>
2026-03-07 23:10:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect}from"react";
-import{PU,BG,WH,BR,DK,BL,HDR,MHE,MCTX,YEARS,TEN_LOGO,DV_LOGO}from"./constants.js";
+import{PU,BG,WH,BR,DK,BL,HDR,MHE,getMCTX,YEARS,TEN_LOGO,DV_LOGO}from"./constants.js";
 import{SLogo,Badge,dayName,callAI,buildPrompt,saveGantt,loadGantt,listGantts}from"./utils.jsx";
 import PostCard from"./PostCard.jsx";
 
